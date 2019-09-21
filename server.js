ed659edd2bed685b119a1cdc8cd544c7e475a525
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-21 15:50:02 -0400

    added mongoose and bodyParser

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
 const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 const Port = process.env.PORT || 4000;
 
-app.listen(Port, () => {
-  console.log(`Currently running on Port: ${Port}`);
+app.listen(Port, (err) => {
+    if (err) throw err
+  console.log(chalk.blue.inverse(`Currently running on Port: ${Port}`));
 });
