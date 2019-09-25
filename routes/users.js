baf5d52fea9e3d11b1e4043fc5ff68de1d5d4e41
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-24 23:01:13 -0400

    updated users routes

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,19 @@ const router = express.Router();
 // Load the User Model Schema
 const User = require("../models/User");
 
-// @route GET
+// @route GET api/users/test
 // @desc Test Route
-// Access:
-
+// Access: Public
 router.get("/test", (req, res) => {
   res.json({ msg: "Route is working" });
 });
 
+
+// @route POST api/users/register
+// @desc Register
+// Public
+
+router.post("/register", (req, res) => {
+    
+})
 module.exports = router;
