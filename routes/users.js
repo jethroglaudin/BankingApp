cceb0f984941610ddb4fb4efbe76ce1821245447
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-27 21:55:04 -0400

    saved newUser payload to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs")
 // Load the User Model Schema
 const User = require("../models/User");
 
@@ -23,6 +23,12 @@ router.post("/register", (req, res) => {
     if(user){
       return res.status(400).json({"message": "User already exists" })
     }
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    newUser.save().then(user => res.json(user))
   })
 })
 module.exports = router;
