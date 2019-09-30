b6e4612a441f964e4d3b05fc69448f317842871e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-29 20:58:04 -0400

    initialized cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Initiate cors for Cross Origin Resource Sharing
+// Initialize cors for Cross Origin Resource Sharing
 app.use(cors())
 
 
