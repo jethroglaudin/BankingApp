fdcc1d5f036122a29be5b24048366521b5b57531
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-02 23:16:12 -0400

    began validation handling wiether the data we receive is empty or populated

--- validations/register.js
+++ validations/register.js
@@ -1,4 +1,16 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-// 
+module.exports = function validateRegistration(data) {
+  let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
